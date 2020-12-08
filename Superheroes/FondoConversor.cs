e1bcc76be6d8771f4b5433c4ed75916c3042d88f
SusanRojas <sf.rojas16@iesdoctorbalmis.com>
2020-12-08 01:27:25 +0100

    Agregar archivos de proyecto.

---
+++ Superheroes/FondoConversor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Superheroes
+{
+    class FondoConversor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Boolean)value) return "PaleGreen";
+            else return "IndianRed";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Superheroes/VisibilidadConversor.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+namespace Superheroes
+{
+    class VisibilidadConversor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((Boolean)value == true) return Visibility.Visible;
+            else return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
